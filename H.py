c9ab90b8daf95aad7d6a37fa4418ae27c85a89b9
dubejeanalex <alexandre.dubejean@live.ca>
2019-03-27 14:21:12 -0400

    Voici les lettres H et I

---
+++ H.py
@@ -0,0 +1,29 @@
+# Ceci est la lettre H
+from gpiozero import LED
+from time import sleep
+
+led=LED(17)
+
+led.on()
+sleep(1)
+led.off()
+sleep(1)
+
+led.on()
+sleep(1)
+led.off()
+sleep(1)
+
+led.on()
+sleep(1)
+led.off()
+sleep(1)
+
+led.on()
+sleep(1)
+led.off()
+
+
+
+
+
