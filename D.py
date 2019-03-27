92aa28474f831006021142086034257a44b4dacc
dubejeanalex <alexandre.dubejean@live.ca>
2019-03-27 14:11:49 -0400

    Voici la lettre D

---
+++ D.py
@@ -0,0 +1,24 @@
+# Ceci est la lettre D
+from gpiozero import LED
+from time import sleep
+
+led=LED(17)
+
+led.on()
+sleep(3)
+
+led.off()
+sleep(1)
+
+led.on()
+sleep(1)
+led.off()
+sleep(1)
+
+led.on()
+sleep(1)
+
+led.off()
+
+
+
